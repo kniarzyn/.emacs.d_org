d8a68769d3b548a4342b3b5921d25acc2160e17e
Paweł Kwietniewski <pkwietniewski@pulawy.com>
2019-04-26 10:50:02 +0200

    WIP: Organize konfiguration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/")
+	     '("org" . "https://orgmode.org/elpa/"))
 ;; (add-to-list 'package-archives
 ;; 	     '("melpa2" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
 ;; (add-to-list 'package-archives
@@ -13,15 +14,13 @@
 	(package-refresh-contents)
 	(package-install 'use-package))
 
-
-
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ivy which-key use-package))))
+ '(package-selected-packages (quote (enh-ruby-mode magit which-key use-package ivy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
